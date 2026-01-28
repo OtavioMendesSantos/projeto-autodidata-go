3b32dd6a38e72a4417e9ce667ef859754737d560
Otávio Mendes Santos <otaviomendessantos2019@gmail.com>
2026-01-28 10:32:14 -0300

    course: A palavra-chave var

--- code/02-variaveis-valores-e-tipos/main.go
+++ code/02-variaveis-valores-e-tipos/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+var globalVariable = "Essa variável tem escopo de pacote"
+// Variável declarada em um code block é undefined em outro
+// Para variáveis com uma abrangência maior, package level scope, utilizamos var
+// Funciona em qualquer lugar
+
 func main() {
 	// := | Declara ao menos uma nova variável e atribui valor | Se usa apenas na primeira vez criando a variável
 	// =  | Apenas reatribui valor a variável existente        | Se usa apenas caso a variável já foi declarada
@@ -15,4 +20,6 @@ func main() {
 	// %d = formato de int, %s = formata string, %v = valor padrão, \n = quebra linha
 
 	fmt.Println(xIsGreaterThanY)
+
+	otherFunc();
 }
---
+++ code/02-variaveis-valores-e-tipos/other-func.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func otherFunc() {
+	fmt.Println("[Other func] Imprimindo variável global: ", globalVariable)
+}
