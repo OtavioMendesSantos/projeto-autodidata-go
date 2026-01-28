12957b551b87f39cee988285764667a396cdf20f
Otávio Mendes Santos <otaviomendessantos2019@gmail.com>
2026-01-28 10:21:46 -0300

    course: Operador curto de declaração

---
+++ code/02-variaveis-valores-e-tipos/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// := | Declara ao menos uma nova variável e atribui valor | Se usa apenas na primeira vez criando a variável
+	// =  | Apenas reatribui valor a variável existente        | Se usa apenas caso a variável já foi declarada
+
+	x := 10                  // Declara int x = 10
+	y := 20                  // Declara int y = 20
+	z := x + y               // z = 30 (int)
+	xIsGreaterThanY := x > y // xIsGreaterThanY = false (bool)
+
+	fmt.Printf("%d %v, %T\n", z, z, z)
+	// %d = formato de int, %s = formata string, %v = valor padrão, \n = quebra linha
+
+	fmt.Println(xIsGreaterThanY)
+}
