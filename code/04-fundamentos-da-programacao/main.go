1c64d202f788489d509ad4a91660ba9aa0b9df6f
Otávio Mendes Santos <otaviomendessantos2019@gmail.com>
2026-02-02 11:59:45 -0300

    course: Tipos numéricos

--- code/04-fundamentos-da-programacao/main.go
+++ code/04-fundamentos-da-programacao/main.go
@@ -1,10 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 var x bool
 
+// Tipos Inteiros
+// - uint8: Conjunto de todos os inteiros sem sinal de 8 bits (0 a 255).
+// - uint16: Conjunto de todos os inteiros sem sinal de 16 bits (0 a 65.535).
+// - int8: Conjunto de todos os inteiros com sinal de 8 bits (-128 a 127).
+// - int16: Conjunto de todos os inteiros com sinal de 16 bits (-32.768 a 32.767).
+// - int64: Conjunto de todos os inteiros com sinal de 64 bits (faixa longa: -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807).
+
+// Tipos Float e Complexos
+// - float32: Conjunto de todos os números de ponto flutuante IEEE-754 de 32 bits.
+// - complex64: Conjunto de todos os números complexos com partes real e imaginária float32.
+// - byte: Aliás para int8.
+// - rune: Aliás para int32.​
+
 func main() {
+	// valuesBool()
+	// valuesByte()
+	valuesInt()
+	myComputer()
+}
+
+func valuesBool() {
 	fmt.Println("Zero value of bool:", x)
 	x = true
 	fmt.Println("New value of bool:", x)
@@ -13,4 +36,29 @@ func main() {
 	z := 10 > 100
 	w := 10 != 100
 	fmt.Println(x, y, z, w)
+}
+
+func valuesByte() {
+	a := "e"
+	b := "é"
+	c := "是"
+	fmt.Printf("%v\t%v\t%v\n", a, b, c)
+
+	d := []byte(a)
+	e := []byte(b)
+	f := []byte(c)
+	fmt.Printf("%v\t%v\t%v\n", d, e, f)
+}
+
+func valuesInt() {
+	x := 10
+	y := 10.0
+	fmt.Printf("%v, %T\n", x, x)
+	fmt.Printf("%v, %T\n", y, y)
+}
+
+func myComputer() {
+	fmt.Println(runtime.GOOS)
+	fmt.Println(runtime.GOARCH)
+	fmt.Println(runtime.NumCPU())
 }
