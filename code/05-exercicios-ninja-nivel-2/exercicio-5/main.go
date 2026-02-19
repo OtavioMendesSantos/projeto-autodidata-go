3381855a2313d57f858dc91925b0a266755130da
Otávio Mendes Santos <otaviomendessantos2019@gmail.com>
2026-02-19 14:49:31 -0300

    course: Nivel 2 - exercício 5

---
+++ code/05-exercicios-ninja-nivel-2/exercicio-5/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Crie uma variável de tipo string utilizando uma raw string literal.
+// Demonstre-a.
+
+func main() {
+	x := `
+b
+	i
+		z
+			a
+				r
+					r
+						o
+	`
+	fmt.Println(x)
+}
