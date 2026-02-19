88ca7c4438e0f67271447d605bc9f048e36b1b3d
Otávio Mendes Santos <otaviomendessantos2019@gmail.com>
2026-02-19 16:26:36 -0300

    course: a declaração for

--- code/06-fluxo-de-controle/main.go
+++ code/06-fluxo-de-controle/main.go
@@ -1,15 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
 	// for x := 0; x < 10; x++ {fmt.Println(x)}; -> for loop can be modernized using range over int
+	// Go não tem while
+
 	total := 0
-	for i := range 10 {
+	for i := range 100 {
 		for x := range 10 {
 			fmt.Printf("Primeiro loop %v, Segundo loop %v\n", i, x)
 			total++
 		}
 	}
 	fmt.Printf("===== Finalizou com %v repetições =====\n", total)
+
+	// Infinite loop
+	// for {
+	// 	fmt.Println("Executando...")
+	// }
+
+	x := 0
+	y := 0
+
+	fmt.Println("Iniciando processamento desnecessário...")
+	for {
+		if x >= 10 {
+			break
+		}
+		random := rand.Intn(51) + 1
+		y++
+		if random > 50 {
+			fmt.Printf("Entrando +1 para a conta (%v na tentativa %v)\n", x, y)
+			x++
+		}
+	}
+	fmt.Printf("===== Após %v tentativas, o for acabou =====\n", y)
 }
