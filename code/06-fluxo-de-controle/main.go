b1e34cdbba926ec7c3146df2d886b9988d920e04
Otávio Mendes Santos <otaviomendessantos2019@gmail.com>
2026-02-19 18:17:11 -0300

    course: declaração switch

--- code/06-fluxo-de-controle/main.go
+++ code/06-fluxo-de-controle/main.go
@@ -74,7 +74,8 @@ func desafio() {
 }
 
 func condicional() {
-	if x := 10; x > 5 {
+	x := 10
+	if x > 5 {
 		fmt.Println("Maior que 5")
 	} else {
 		fmt.Println("Menor que 5")
@@ -82,4 +83,36 @@ func condicional() {
 	// if err := doSomething(); err != nil {
 	// 	fmt.Println(err)
 	// }
+
+	switch {
+	case x < 5:
+		fmt.Println("Menor que 5")
+	case x > 5:
+		fmt.Println("Maior que 5")
+	default:
+		fmt.Println("Igual a 5")
+	}
+
+	fruta := "banana"
+	switch fruta {
+	case "maçã":
+		fmt.Println("Maçã")
+	case "banana":
+		fmt.Println("Banana")
+		fallthrough // cair através
+	case "granola":
+		fmt.Println("com granola é massa")
+	default:
+		fmt.Println("Fruta desconhecida")
+	}
+
+	equipe := "Azul"
+	switch equipe {
+	case "Azul", "Vermelho", "Amarelo":
+		fmt.Println("Equipe Azul, Vermelho ou Amarelo")
+	case "Verde":
+		fmt.Println("Equipe Verde")
+	default:
+		fmt.Println("Equipe desconhecida")
+	}
 }
