0ee5b72c429463df89d3f5a9ccfc321400e858d3
Otávio Mendes Santos <otaviomendessantos2019@gmail.com>
2026-02-19 16:09:31 -0300

    course: nested loops

--- code/06-fluxo-de-controle/main.go
+++ code/06-fluxo-de-controle/main.go
@@ -4,8 +4,12 @@ import "fmt"
 
 func main() {
 	// for x := 0; x < 10; x++ {fmt.Println(x)}; -> for loop can be modernized using range over int
+	total := 0
 	for i := range 10 {
-		fmt.Println(i)
+		for x := range 10 {
+			fmt.Printf("Primeiro loop %v, Segundo loop %v\n", i, x)
+			total++
+		}
 	}
-
+	fmt.Printf("===== Finalizou com %v repetições =====\n", total)
 }
